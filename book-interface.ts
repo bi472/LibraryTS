06152ade9a33f41dedccf6bd8ac7190cf047de13
fnder7 <matt.nolimiteron@gmail.com>
2023-05-03 19:47:48 +0300

    inital commit

---
+++ book-interface.ts
@@ -0,0 +1,10 @@
+interface Book {
+    id: "string",
+    title: "string",
+    description: "string",
+    authors: "string",
+    favorite: "string",
+    fileCover: "string",
+    fileName: "string"
+}
+
---
+++ book-repository.ts
@@ -0,0 +1,11 @@
+abstract class BookRepository {
+    constructor() {
+
+    }
+
+    createBook(book: Book){}
+    getBook(id: number){}
+    getBooks(){} 
+    updateBook(id: number){}
+    deleteBook(id: number){}
+}
